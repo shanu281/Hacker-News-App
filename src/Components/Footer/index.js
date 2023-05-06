9dcd2a980ea130118fc8f7679cc884bdfead0333
Shanu Kumar <shanukumar467@gmail.com>
2023-05-06 11:46:38 +0530

    Header added

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black">
+      <div className="text-white  py-4 text-center">
+        Copyright &copy; All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

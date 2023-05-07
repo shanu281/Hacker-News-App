3ec2b5e9ab2726978eefd941a638e704db4e4972
Shanu Kumar <shanukumar467@gmail.com>
2023-05-07 15:18:04 +0530

    Search input addded to home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BsSearch } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className=" container h-[750px] mx-auto shadow-2xl">
+      <div className=" mx-auto flex flex-col items-center gap-x-2">
+      
 
-export default Home
+        <input
+          type="search"
+          placeholder="Search news..."
+          className="rounded-full mx-auto  w-1/2  font-semibold px-6 py-4 lg:shadow-xl shadow-2xl"
+          />
+        {/* <div>
+          <BsSearch className="text-3xl" />
+        </div> */}
+          
+      <div className="my-3 ">
+        <button className="btn btn-lg btn-primary w-[100px]  rounded bg-gray-400  p-3 text-white hover:bg-slate-300 hover:text-black  transition shadow-2xl ">
+          Search
+        </button>
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
